e2f111cba4af6b224999ef28057cadd19353dc2c
Avi Patel <avi.patel0025@gmail.com>
2021-03-02 12:45:29 -0500

    test deploy

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div className="container">
       <div className="logo">
         <NavLink className="option" to="/">
-          avi pate
+          avi patel
         </NavLink>
       </div>
 
