2cf39a050ef7a60eae1c53b8f309d8356ee4940d
Avi Patel <avi.patel0025@gmail.com>
2021-03-02 10:25:19 -0500

    Modified placement of contactOverlay on about page and adjusted footer

--- src/pages/about-page/about-page.component.jsx
+++ src/pages/about-page/about-page.component.jsx
@@ -5,6 +5,8 @@ import ProfilePicture from "../../assets/images/profile-picture.JPG";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import Footer from "../../components/footer/footer.component";
 
+import ContactPanel from "../../components/contact-panel/contact-panel.component";
+
 import { ReactComponent as Triangle1 } from "../../assets/icons/triangle1.svg";
 import { ReactComponent as Triangle2 } from "../../assets/icons/triangle2.svg";
 import { ReactComponent as Triangle3 } from "../../assets/icons/triangle3.svg";
@@ -12,48 +14,60 @@ import { ReactComponent as Triangle4 } from "../../assets/icons/triangle4.svg";
 import { ReactComponent as Triangle5 } from "../../assets/icons/triangle5.svg";
 import { ReactComponent as Triangle6 } from "../../assets/icons/triangle6.svg";
 
-const AboutPage = () => (
-  <div className="aboutPage">
-    <div className="imageSection">
-      <h2 className="greeting">Hello!</h2>
-      <div className="imageContainer">
-        <img class="image" src={ProfilePicture} alt="Profile Picture" />
+const AboutPage = () => {
+  function handleContactPrompt(e) {
+    e.preventDefault();
+    console.log("Contact Button pressed");
+    document.querySelector(".contactPanel").classList.toggle("visible");
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  return (
+    <div className="aboutPage darken">
+      <div className="imageSection">
+        <h2 className="greeting">Hello!</h2>
+        <div className="imageContainer">
+          <img class="image" src={ProfilePicture} alt="Profile Picture" />
+        </div>
+        <Triangle4 className="triangle triangle3" />
+        <Triangle3 className="triangle triangle4" />
       </div>
-      <Triangle4 className="triangle triangle3" />
-      <Triangle3 className="triangle triangle4" />
-    </div>
 
-    <div className="content">
-      <Triangle1 className="triangle triangle1" />
-      <Triangle2 className="triangle triangle2" />
-      <h3 className="heading">I'm Avi!</h3>
-      <h5 className="tagline">Developer // Photographer</h5>
-      <p>
-        I’m a second year Computer Science student at the University of Waterloo
-        and I love creating things and bringing my ideas to life. I am driven by
-        the desire to create positive change and I love getting to know new
-        people.
-      </p>
-      <p>
-        Other passions of mine include coding, watching anime, taking pictures
-        and going on long bike rides or road trips.
-      </p>
-      <p>
-        Thanks for visiting, hope you enjoyed my site! If you have any questions
-        or concerns or would like to get to know me better, feel free to reach
-        out below! I’m always up for a cup of coffee.
-      </p>
-      <p>Email: avi.patel0025@gmail.com</p>
+      <div className="content">
+        <Triangle1 className="triangle triangle1" />
+        <Triangle2 className="triangle triangle2" />
+        <h3 className="heading">I'm Avi!</h3>
+        <h5 className="tagline">Developer // Photographer</h5>
+        <p>
+          I’m a second year Computer Science student at the University of
+          Waterloo and I love creating things and bringing my ideas to life. I
+          am driven by the desire to create positive change and I love getting
+          to know new people.
+        </p>
+        <p>
+          Other passions of mine include coding, watching anime, taking pictures
+          and going on long bike rides or road trips.
+        </p>
+        <p>
+          Thanks for visiting, hope you enjoyed my site! If you have any
+          questions or concerns or would like to get to know me better, feel
+          free to reach out below! I’m always up for a cup of coffee.
+        </p>
+        <p>Email: avi.patel0025@gmail.com</p>
 
-      <Triangle5 className="triangle triangle5" />
-      <Triangle6 className="triangle triangle6" />
+        <Triangle5 className="triangle triangle5" />
+        <Triangle6 className="triangle triangle6" />
 
-      <div className="buttonContainer">
-        <CustomButton accent>Contact</CustomButton>
+        <div className="buttonContainer">
+          <CustomButton onClick={handleContactPrompt} accent>
+            Contact
+          </CustomButton>
+        </div>
       </div>
+      <ContactPanel id="contactPanel" />
       <Footer />
     </div>
-  </div>
-);
+  );
+};
 
 export default AboutPage;
