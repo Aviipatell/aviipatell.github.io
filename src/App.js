ba47ca9c1254934f35115b811c11c86d2272845e
Avi Patel <avi.patel0025@gmail.com>
2022-01-28 02:41:45 -0500

    create react app and added folder structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React from "react";
-import "./App.scss";
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
-
-import HomePage from "./pages/home-page/home-page.component";
-import AboutPage from "./pages/about-page/about-page.component";
-
-import Header from "./components/header/header.component";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
