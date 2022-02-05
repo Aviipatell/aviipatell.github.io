08cbad206e1eae01848e76d75dc1e666df40842f
Avi Patel <avi.patel0025@gmail.com>
2022-02-04 23:57:28 -0500

    set up playground component

---
+++ src/components/playground/playground.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./playground.styles.css";
+
+const Playground = () => {
+  return (
+    <div className="playground-container">
+      <p>Playground</p>
+    </div>
+  );
+};
+
+export default Playground;
