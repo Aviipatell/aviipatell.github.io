10faf417400147f9d68fa0babb7d28e31d6b066e
Avi Patel <avi.patel0025@gmail.com>
2022-02-10 22:55:16 -0500

    updated gaming page

--- src/pages/gaming-page/gaming-page.component.jsx
+++ src/pages/gaming-page/gaming-page.component.jsx
@@ -6,6 +6,11 @@ const GamingPage = () => {
   return (
     <div className="gaming-page">
       <h1>Gaming Page</h1>
+      <p>
+        Section of games which users can play or interact with and opt in to
+        login to save their highscores which will be displayed OR play as a
+        guest instead, up to them.
+      </p>
     </div>
   );
 };
